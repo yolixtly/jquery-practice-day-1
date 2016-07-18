f6bbd5edb8c4e358356c28b4af60d7c52574d214
yolixtly <yolixtly_13@hotmail.com>
2016-07-18 11:49:26 -0400

    first practice

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('#wrapper').css('width', '500px').css('background-color', 'steelblue')
+	.css('color', 'mintcream').css('margin', '0 auto');
+
+	$('h1').css('font-size', '3em').css('text-align', 'center');
+
+	$('p').css('text-align', 'justified').css('padding', '1em').css('background-color', 'cornflowerblue');
+
+	$('.highlighted').css('background-color', 'indianred');
+});
